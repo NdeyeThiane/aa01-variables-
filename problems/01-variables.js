67f10b2ecc10ad2ab7c7505fba2c7b9ced0c65ad
Ndeye <ndeyet2004@gmail.com>
2024-05-13 16:33:59 -0700

    Done with variables homework

--- problems/01-variables.js
+++ problems/01-variables.js
@@ -6,35 +6,50 @@ test your work by running your code and the test specs!
 // 1. Declare a variable called `firstName` and assign it your first name
 //    as a string:
 
-// Your code here 
+// Your code here
+
+let firstName = "Ndeye";
 
 
 // 2. Declare a variable called `lastName` and assign it your last name
 //    as a string:
 
-// Your code here 
+// Your code here
+
+let lastName = "Thiane";
 
 
 // 3. Declare a variable called `age`:
 
-// Your code here 
+// Your code here
+
+let age;
 
 
 // 4. Print out the `firstName`, `lastName`, and `age` variables. What
 //    do you expect them to be when they get printed?
 
-// Your code here 
+// Your code here
 
+console.log(firstName);
+console.log(lastName);
+console.log(age);
 
 // 5. Assign the `age` variable to a number:
 
-// Your code here 
+// Your code here
+
+age = "19"
 
 
 // 6. Print out the `firstName`, `lastName`, and `age` variables. What
 //    do you expect them to be when they get printed?
 
-// Your code here 
+// Your code here
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
 
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
